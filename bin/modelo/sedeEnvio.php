872e15381871552acba2b09321b55fbd8314d7c6
Ricardo Parra <ricardo.prxr16@gmail.com>
2023-07-25 01:04:35 -0400

    Módulos sede envíos hecho

---
+++ bin/modelo/sedeEnvio.php
@@ -0,0 +1,218 @@
+<?php  
+
+	namespace modelo;
+	use config\connect\DBConnect as DBConnect;
+
+	class sedeEnvio extends DBConnect{
+
+		private $empresa;
+		private $ubicacion;
+		private $id;
+
+		public function __construct(){
+			parent::__construct();    
+		}
+
+		public function mostrarSedes($bitacora = false){
+			try{
+				$sql = "SELECT s.id_sede, e.nombre, s.ubicacion, s.id_empresa FROM sede_envio s
+						INNER JOIN empresa_envio e ON s.id_empresa = e.id_empresa
+						WHERE s.status = 1;";
+				$new = $this->con->prepare($sql);
+				$new->execute();
+				$data = $new->fetchAll(\PDO::FETCH_OBJ);
+				echo json_encode($data);
+				if($bitacora) $this->binnacle("Sede de envío",$_SESSION['cedula'],"Consultó listado.");
+				die();
+
+			}catch(\PDOException $e){
+				return $e;
+			}
+		}
+
+		public function selectEmpresas(){
+			try{
+
+				$sql = "SELECT * FROM empresa_envio WHERE status = 1";
+				$new = $this->con->prepare($sql);
+				$new->execute();
+				return $new->fetchAll(\PDO::FETCH_OBJ);
+
+			}catch(\PDOException $e){
+				die($e);
+			}
+		}
+
+		public function validarEmpresa($empresa){
+			$this->empresa = $empresa;
+			try {
+				
+				$new = $this->con->prepare('SELECT * FROM empresa_envio WHERE id_empresa = ? AND status = 1');
+				$new->bindValue(1,$this->empresa);
+				$new->execute();
+				$data = $new->fetchAll(\PDO::FETCH_OBJ);
+
+				return isset($data[0]->id_empresa);
+
+			} catch (\PDOException $e) {
+				die($e);
+			}
+		}
+
+		public function getRegistrarSede($empresa, $ubicacion){
+
+			if(preg_match_all('/^[a-zA-ZÀ-ÿ0-9\s,.\-\/\'"#]{7,70}$/', $ubicacion) != 1){
+				echo json_encode(['resultado' => 'Error de sede','error' => 'Direccion inválida.']);
+				die();
+			}
+			if(preg_match_all("/^[0-9]{1,5}$/", $empresa) != 1){
+				echo json_encode(['resultado' => 'Error de empresa','error' => 'Empresa inválido.']);
+				die();
+			}
+
+			$this->empresa = $empresa;
+			$this->ubicacion = $ubicacion;
+
+			if($this->validarEmpresa($this->empresa)){
+				$this->registrarSede();
+			}else{
+				die(json_encode(['resultado' => false, 'msg' => 'La empresa no existe.']));
+			}
+		}
+
+		private function registrarSede(){
+
+			try{
+
+				$new = $this->con->prepare("INSERT INTO sede_envio(id_sede, ubicacion, id_empresa, status) VALUES (DEFAULT,?, ?, 1)");
+				$new->bindValue(1, $this->ubicacion); 
+				$new->bindValue(2, $this->empresa); 
+				$new->execute();
+
+				$resultado = ['resultado' => true, 'msg' => 'Laboratorio registrado.'];
+				echo json_encode($resultado);
+				$this->binnacle("Sede de envío",$_SESSION['cedula'],"Registró sede de envío .");
+				die();
+
+
+			}catch(\PDOException $error){
+				return $error;
+			} 
+
+		}
+
+		public function getSede($id){
+
+			if(preg_match_all("/^[0-9]{1,10}$/", $id) != 1){
+				echo json_encode(['resultado' => 'Error de id','error' => 'Id inválida.']);
+				die();
+			}
+
+			$this->id = $id;
+
+			return $this->selectSede();
+		}
+
+		private function selectSede(){
+
+			try{
+				$new = $this->con->prepare("SELECT * FROM sede_envio WHERE id_sede = ? AND status = 1;");
+				$new->bindValue(1, $this->id);
+				$new->execute();
+				$data = $new->fetchAll(\PDO::FETCH_OBJ);
+
+				if(!isset($data[0])){
+					return false;
+				}else{
+					return $data[0];
+				}
+
+			}catch(\PDOException $e){
+				return $e;
+			}
+
+		}
+
+		public function getEditarSede($empresa, $ubicacion, $id){
+
+			if(preg_match_all('/^[a-zA-ZÀ-ÿ0-9\s,.\-\/\'"#]{7,70}$/', $ubicacion) != 1){
+				echo json_encode(['resultado' => 'Error de sede','error' => 'Direccion inválida.']);
+				die();
+			}
+			if(preg_match_all("/^[0-9]{1,5}$/", $empresa) != 1){
+				echo json_encode(['resultado' => 'Error de empresa','error' => 'Empresa inválido.']);
+				die();
+			}
+
+			$this->empresa = $empresa;
+			$this->ubicacion = $ubicacion;
+			$this->id = $id;
+
+			if($this->validarEmpresa($this->empresa)){
+				if($this->selectSede() != false){
+					$this->editarSede();
+				}
+			}else{
+				die(json_encode(['resultado' => false, 'msg' => 'La empresa no existe.']));
+			}
+
+		}
+
+		private function editarSede(){
+
+			try{
+
+				$new = $this->con->prepare("
+					UPDATE sede_envio SET ubicacion = ?, id_empresa = ? 
+					WHERE id_sede = ?;");
+				$new->bindValue(1, $this->ubicacion);
+				$new->bindValue(2, $this->empresa);
+				$new->bindValue(3, $this->id);
+				$new->execute();
+
+				$this->binnacle("Sede de envío",$_SESSION['cedula'],"Editó sede de envío.");
+				$resultado = ['resultado' => true, 'msg' => 'Sede de envío editada.'];
+				die(json_encode($resultado));
+
+			}catch(\PDOException $error){
+				echo json_encode($error);
+				die();
+			}
+
+		} 
+
+
+		public function getEliminarSede($id){
+			if(preg_match_all("/^[0-9]{1,10}$/", $id) != 1){
+				echo json_encode(['resultado' => 'Error de id','error' => 'Id inválida.']);
+				die();
+			}
+
+			$this->id = $id;
+
+			if($this->selectSede() != false){
+				$this->eliminarSede();
+			}else {
+				die(json_encode(['resultado' => true, 'msg' => 'La sede de envío no existe.']));
+			}
+		}
+
+		private function eliminarSede(){
+			try{
+
+				$new = $this->con->prepare("UPDATE sede_envio SET status = 0 WHERE id_sede = ?;");
+				$new->bindValue(1, $this->id);
+				$new->execute();
+				$this->binnacle("Sede de envío",$_SESSION['cedula'],"Eliminó sede de envío.");
+				$resultado = ['resultado' => true, 'msg' => 'Se eliminó la sede correctamente.'];
+				die(json_encode($resultado));
+
+			}catch(\PDOException $e){
+				return $e;
+			}
+
+		}
+
+	}
+
+?>
