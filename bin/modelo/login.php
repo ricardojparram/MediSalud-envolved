69fa80117a8c07f30130534d144946bacc57ee7d
Ricardo Parra <ricardo.prxr16@gmail.com>
2023-10-15 21:56:46 -0400

    Login actualizado para conectar y desconectar bd

--- bin/modelo/login.php
+++ bin/modelo/login.php
@@ -5,13 +5,12 @@
 
 
 	class login extends DBConnect{
+
 		private $cedula;
 		private $password;
 
-
 		public function __construct(){
 			parent::__construct();
-			
 		}
 		
 		public function getLoginSistema($cedula ,$password){
@@ -29,12 +28,13 @@
 			$this->cedula = $cedula;
 			$this->password = $password;
 			
-			return $this->loginSistema();
+			$this->loginSistema();
 		}
 
 		private function loginSistema(){
 
 			try{
+				$this->conectarDB();
 				$new = $this->con->prepare("SELECT u.cedula, u.nombre, u.apellido, u.correo, u.password, u.img, u.rol as nivel, r.nombre as puesto FROM usuario u 
 					INNER JOIN rol r
 					ON r.id_rol = u.rol
@@ -43,33 +43,28 @@
 				$new->execute();
 				$data = $new->fetchAll();
 
-				if(isset($data[0]["password"])){
-					if(password_verify($this->password, $data[0]['password'])){
-
-						$_SESSION['cedula'] = $data[0]['cedula'];
-						$_SESSION['nombre'] = $data[0]['nombre'];
-						$_SESSION['apellido'] = $data[0]['apellido'];
-						$_SESSION['correo'] = $data[0]['correo'];
-						$_SESSION['nivel'] = $data[0]['nivel'];
-						$_SESSION['puesto'] = $data[0]['puesto'];
-						$_SESSION['fotoPerfil'] = (isset($data[0]['img'])) ? $data[0]['img'] : 'assets/img/profile_photo.jpg';
-
-						
-						$resultado = ['resultado' => 'Logueado'];
-						echo json_encode($_SESSION);
-						die();
-
-						
-					}else{
-						$resultado = ['resultado' => 'Error de contraseña' , 'error' => 'Contraseña incorrecta.'];
-						echo json_encode($resultado);
-						die();
-					}
-				}else{
+				if(!isset($data[0]["password"])){
 					$resultado = ['resultado' => 'Error de cedula', 'error' => 'La cédula no está registrada.'];
-					echo json_encode($resultado);
-					die();
+					$this->desconectarDB();
+					die(json_encode($resultado));
 				}
+				if(!password_verify($this->password, $data[0]['password'])){
+					$resultado = ['resultado' => 'Error de contraseña' , 'error' => 'Contraseña incorrecta.'];
+					$this->desconectarDB();
+					die(json_encode($resultado));
+				}
+
+				$_SESSION['cedula'] = $data[0]['cedula'];
+				$_SESSION['nombre'] = $data[0]['nombre'];
+				$_SESSION['apellido'] = $data[0]['apellido'];
+				$_SESSION['correo'] = $data[0]['correo'];
+				$_SESSION['nivel'] = $data[0]['nivel'];
+				$_SESSION['puesto'] = $data[0]['puesto'];
+				$_SESSION['fotoPerfil'] = (isset($data[0]['img'])) ? $data[0]['img'] : 'assets/img/profile_photo.jpg';
+
+				$this->desconectarDB();
+				$resultado = ['resultado' => 'Logueado'];
+				die(json_encode($resultado));
 
 			}catch(PDOException $error){
 				return $error;
