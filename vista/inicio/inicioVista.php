7dbecef7b5236002c857c8c48001392431435d4e
Ricardo Parra <ricardo.prxr16@gmail.com>
2023-10-29 23:16:38 -0400

    Inicio (tienda) actualizada, nuevo frontend, y backend para agregar productos pasado a carrito

--- vista/inicio/inicioVista.php
+++ vista/inicio/inicioVista.php
@@ -106,21 +106,19 @@
         </section><!-- End Services Section -->
 
         <section id="productos" class="productos my-4">
-         <div class="container my-2">
-           <h3 class="text-center p-3 mt-3 fw-bold">Productos</h3>
-           <div class="row mx-auto" id="catalogo">
+          <div class="container my-2">
+            <h3 class="text-center p-3 mt-3 fw-bold">Productos</h3>
+            <div class="row mx-auto justify-content-center align-items-center" id="catalogo">
 
-
-
-           </div>
-           <div class="row">
-            <div class="col-12 d-flex justify-content-center mb-4"> 
-              <a class="btn btn-lg btn-dark" href="#!">Ver Todo</a>
             </div>
-          </div>
+            <div class="row">
+              <div class="col-12 d-flex justify-content-center mb-4"> 
+                <a class="btn btn-lg btn-dark" href="#!"><strong>Ver más...</strong></a>
+              </div>
+            </div>
 
-        </div>
-      </section>
+          </div>
+        </section>
 
 
 
@@ -169,27 +167,77 @@
   </div>
 </div>
 
-<div class="modal fade" id="AñadirCarrito" tabindex="-1" aria-modal="true" role="dialog">
-  <div class="modal-dialog modal-dialog-centered">
+
+
+<div class="modal fade" id="AñadirCarritoModal" style="transition: all 0.4s ease-in-out 0s;" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" role="dialog">
+  <div class="modal-dialog modal-xl modal-dialog-centered">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Añadir a carrito</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <div class="text-center m-3">
-          <img class="card-img-top mx-auto" style="width: 80%;" src="https://images.squarespace-cdn.com/content/v1/58126462bebafbc423916e25/1490212943759-5AVQSBMUSU12111CKAYM/image-asset.png">
+      <div class="modal-body p-8">
+        <div class="position-absolute top-0 end-0 me-3 mt-3">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
+        <div class="row">
+          <div class="col-lg-6 row justify-content-center align-items-center">
+            <!-- img slide -->
+              <img class=".img-fluid" src="assets/img/productos/producto_imagen.png" alt="producto_imagen">
+          </div>
+          <div class="col-lg-6">
+            <div class="ps-lg-8 mt-6 mt-lg-0">
+              <a href="#!" class="mb-4 d-block tipo_medicamento">Antiinflamatorio</a>
+              <h2 class="mb-1 h1 nombre_medicamento">Diclofenac potásico</h2>
+              <h6 class="descripcion_medicamento"></h6>
+              <div class="mb-4">
+                <small class="text-warning">
+                  <i class="bi bi-star-fill"></i>
+                  <i class="bi bi-star-fill"></i>
+                  <i class="bi bi-star-fill"></i>
+                  <i class="bi bi-star-fill"></i>
+                  <i class="bi bi-star-half"></i></small><a href="#" class="ms-2"></a>
+              </div>
+              <div class="fs-4 precios">
+                <span class="fw-bold text-dark precio_bs">Bs. 120.00</span>
+                <span class="text-muted precio_dolar" >$4.00</span>
+              </div>
+              <hr class="my-6">
+              <div class="opciones row">
+                <div class="input-group cantidad_producto col-6" style="max-width: 180px;">
+                  <button class="btn btn-light menos"><i class="bi bi-dash-lg"></i></button>
+                  <input type="number" id="cantidad_añadir_producto" min="1" max="50" value="1" class="form-control form-input">
+                  <button class=" btn btn-light mas"><i class="bi bi-plus-lg"></i></button>
+                </div>
+                <div class="invalid-tooltip-prod col-6" style="">Cantidad no disponible.</div>
+              </div>
+              <div class="mt-3 row justify-content-start g-2 align-items-center">
 
-        <div class="card-title mostrarP p-3">
-
-
+                <div class="col-lg-4 col-md-5 col-6 d-grid">
+                  <button type="button" id="añadir_al_carrito" class="btn btn-success">
+                    Agregar al carrito <i class="bi bi-cart-plus-fill"></i>
+                  </button>
+                </div>
+              </div>
+              <hr class="my-6">
+              <div>
+                <table class="table table-borderless">
+                  <tbody>
+                    <tr>
+                      <td>Código de producto:</td>
+                      <td class="codigo_producto"></td>
+                    </tr>
+                    <tr>
+                      <td>Tipo:</td>
+                      <td class="tipo_producto"></td>
+                    </tr>
+                    <tr>
+                      <td>Contraindicaciones:</td>
+                      <td class="contraindicaciones">
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary cerrar" data-bs-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-success" id="añadirAlCarrito">Añadir al carrito</button>
-
       </div>
     </div>
   </div>
